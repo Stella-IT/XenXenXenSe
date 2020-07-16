d7faba30e4dfefa1f57147371fa02aab5d486e27
Alex4386 <psh010209@gmail.com>
2020-07-16 11:24:36 +0900

    chore(refactor): split file with core and main

---
+++ core.py
@@ -0,0 +1,92 @@
+import uvicorn
+from API.v1 import router as _v1_router
+from threading import Thread
+
+# Temp solution
+from MySQL import init_connection
+
+class XenXenXenSeCore:
+    def __init__(self, app, xen_credentials):
+        self.xen_credentials = xen_credentials
+        self.terminating = False
+        self.app = app
+
+        # include API router
+        self.app.include_router(_v1_router)
+
+    @classmethod
+    def show_banner(self, add_padding=False):
+        ''' Show banner for XenXenXenSe Project '''
+        from pyfiglet import Figlet
+        figlet = Figlet()
+
+        print(figlet.renderText("XenXenXenSe"))
+        print("Project XenXenXenSe : a RESTful API implementation for Citrix Hypervisor® and XCP-ng")
+        print()
+        print("Copyright (c) Stella IT.")
+        print("This software is distributed under Affero GNU Public License v3.")
+        
+        if add_padding:
+            print()
+
+    def print_xen_hostnames(self, show_title=False):
+        ''' Print Xen Hostnames to screen '''
+        if show_title:
+            print("Detected Clusters")
+
+        for credentials in self.xen_credentials:
+            print("*", credentials)
+
+    
+    def run_api_server(self, development_mode=False):
+        ''' Run API Server '''
+        if development_mode:
+            # development environment
+            print("Running in development mode!")
+            uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+        else:
+            # production environment
+            uvicorn.run(self.app, host="127.0.0.1", port=8000)
+
+    def connect_db(self):
+
+        # Temporary Solution, will refactor to OOP Python. - @zeroday0619 Plz help!
+        init_connection()
+
+
+    def schedule_process(self):
+        ''' The Thread content to run on scheduler '''
+        print("Data Caching Schedule handling has been started!")
+        print()
+        try:
+            while not self.terminating:
+                schedule.run_pending()
+                time.sleep(1)
+        except:
+            print("Exception was detected")
+            self.terminating = True
+
+        print()
+        print("Schedule handling is terminating!")
+
+    def start(self):
+        self.show_banner(True)
+        self.print_xen_hostnames(True)
+        print()
+
+        # Detect if server is executed with development mode
+        development_mode = (("-d" in sys.argv) or ("--dev" in sys.argv))
+
+        # Create new Thread
+        schedule_thread = Thread(target=self.schedule_process)
+        schedule_thread.start()
+
+        # Run API Server
+        self.run_server(development_mode)
+
+        # Termination
+        self.terminating = True
+
+        # merge with schedule thread
+        schedule_thread.join()
--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import sys
 
-import uvicorn
 from fastapi import FastAPI
-from threading import Thread
 
 import schedule
 import time
 import signal
 
-from API.v1 import router as _v1_router
-
 from config import xen_credentials
-from MySQL import init_connection
+from core import XenXenXenSeCore
 
 import xmlrpc
 
@@ -31,64 +27,6 @@ app = FastAPI(
     debug=True
 )
 
-app.include_router(_v1_router)
-
-
-class XenXenXenSeCore:
-    terminating = False
-
-    @classmethod
-    def main(cls):
-        # print banner
-        from pyfiglet import Figlet
-
-        figlet = Figlet()
-
-        print(figlet.renderText("XenXenXenSe"))
-        print("Project XenXenXenSe : a RESTful API implementation for Citrix Hypervisor® and XCP-ng")
-        print()
-        print("Copyright (c) Stella IT.")
-        print("This software is distributed under Affero GNU Public License v3.")
-        print()
-        print()
-        print("Detected Clusters")
-        for credentials in xen_credentials:
-            print("*", credentials)
-
-        init_connection()
-        print()
-
-        is_dev = (("-d" in sys.argv) or ("--dev" in sys.argv))
-
-        schedule_thread = Thread(target=cls.schedule_process)
-        schedule_thread.start()
-
-        if is_dev:
-            # development environment
-            print("Running in development mode!")
-            uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-        else:
-            # production environment
-            uvicorn.run(app, host="127.0.0.1", port=8000)
-
-        cls.terminating = True
-        schedule_thread.join()
-
-    @classmethod
-    def schedule_process(cls):
-        print("Data Caching Schedule handling has been started!")
-        print()
-        try:
-            while not cls.terminating:
-                schedule.run_pending()
-                time.sleep(1)
-        except:
-            cls.terminating = True
-
-        print()
-        print("Schedule handling is terminating!")
-
-
 if __name__ == "__main__":
-    XenXenXenSeCore.main()
+    core = XenXenXenSeCore(app, xen_credentials)
+    core.start()
