9bf6ccd853a759df652b7a5fb3acb23a39a90c53
Alex4386 <psh010209@gmail.com>
2021-02-07 16:15:13 +0900

    fix: add endpoint on /v1/

--- API/v1/__init__.py
+++ API/v1/__init__.py
@@ -9,6 +9,10 @@ from API.v1.VDI import vdi_router
 from API.v1.VIF import vif_router
 from API.v1.VM import vm_router
 
+from app.settings import Settings
+
+from pydantic import BaseModel
+
 router = APIRouter()
 
 _prefix = "/v1"
@@ -23,7 +27,25 @@ router.include_router(vif_router, prefix=_prefix)
 router.include_router(vm_router, prefix=_prefix)
 router.include_router(guest_router, prefix=_prefix)
 
+class XXXS_V1_App(BaseModel):
+    version: int
+    name: str
 
-@router.get("/", status_code=404)
-async def root_dir():
-    return {"status_code": 404}
+class XXXS_V1_Root(BaseModel):
+    hello: str
+    app: XXXS_V1_App
+    clusters: List[str]
+
+@router.get("/", response_model=XXXS_V1_Root)
+async def v1_root():
+    xen_clusters = Settings.get_xen_clusters()
+    xen_cluster_names = [xen_cluster for xen_cluster in xen_clusters ]
+    
+    return {
+        "hello": "world",
+        "app": {
+            "version": 1,
+            "name": "XenXenXenSe"
+        },
+        "clusters": xen_cluster_names
+    }
