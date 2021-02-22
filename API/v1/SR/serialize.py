822ead84b107b824bde3554e93ce59ac7d450efe
Alex4386 <psh010209@gmail.com>
2021-02-22 17:34:13 +0900

    chore: make SR also return used and free.

--- API/v1/SR/serialize.py
+++ API/v1/SR/serialize.py
@@ -17,6 +17,8 @@ async def serialize(sr: SR):
         name=sr.get_name(),
         description=sr.get_description(),
         size=sr.get_physical_size(),
+        used=sr.get_physical_utilisation(),
+        free=sr.get_physical_size() - sr.get_physical_utilisation(),
         uuid=sr.get_uuid(),
         content_type=sr.get_content_type(),
         type=sr.get_type(),
