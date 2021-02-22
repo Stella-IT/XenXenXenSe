192b1e2ad200c2cd96bde0db4bb56f51e16f8f1f
Alex4386 <psh010209@gmail.com>
2021-02-22 18:20:20 +0900

    chore: fix invalid string subtraction

--- API/v1/SR/serialize.py
+++ API/v1/SR/serialize.py
@@ -18,7 +18,7 @@ async def serialize(sr: SR):
         description=sr.get_description(),
         size=sr.get_physical_size(),
         used=sr.get_physical_utilisation(),
-        free=sr.get_physical_size() - sr.get_physical_utilisation(),
+        free=str(int(sr.get_physical_size()) - int(sr.get_physical_utilisation())),
         uuid=sr.get_uuid(),
         content_type=sr.get_content_type(),
         type=sr.get_type(),
