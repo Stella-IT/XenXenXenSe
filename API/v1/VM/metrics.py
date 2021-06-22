b2ec350c54e9c8dbba353b33963f1d24d8068187
alex4386 <psh010209@gmail.com>
2021-06-23 02:03:30 +0900

    chore: add options and raw output options

--- API/v1/VM/metrics.py
+++ API/v1/VM/metrics.py
@@ -5,7 +5,7 @@ from http.client import RemoteDisconnected
 from typing import Optional
 
 import requests
-from fastapi import APIRouter, HTTPException, Path
+from fastapi import APIRouter, HTTPException, Path, Response
 
 from app.settings import Settings
 
@@ -17,7 +17,9 @@ async def instance_get_metrics(
     cluster_id: str = Path(default=None, title="cluster_id", description="Cluster ID"),
     vm_uuid: str = Path(default=None, title="vm_uuid", description="VM UUID"),
     start: Optional[int] = None,
+    end: Optional[int] = None,
     interval: Optional[int] = 60,
+    raw: Optional[bool] = False,
 ):
     """Get Xen Host Metrics"""
 
@@ -33,6 +35,9 @@ async def instance_get_metrics(
 
         if start is None:
             start = math.floor(time.time() - interval)
+
+        if end is None:
+            end = math.floor(time.time())
 
         response = session.get(
             url,
@@ -54,27 +59,57 @@ async def instance_get_metrics(
         x_rows = x_data.findall("row")
 
         data = dict()
+        relevant_indeces = []
 
         for i, legend_tag in enumerate(entries):
             name = legend_tag.text
             is_relavant = "vm:" + vm_uuid in name
 
             if is_relavant:
+                relevant_indeces.append(i)
+
                 # data_type = name.split(":")[0]
                 data_name = name.split(":")[3]
 
                 data[data_name] = []
 
-                for row in x_rows:
-                    timestamp = int(row.find("t").text)
-                    value = float(row.findall("v")[i].text)
+                if not raw:
+                    for row in x_rows:
+                        timestamp = int(row.find("t").text)
+                        value = float(row.findall("v")[i].text)
 
-                    data[data_name].append(
-                        dict(
-                            timestamp=timestamp,
-                            value=value,
+                        data[data_name].append(
+                            dict(
+                                timestamp=timestamp,
+                                value=value,
+                            )
                         )
-                    )
+
+        if raw:
+            entries_kill_list = []
+
+            for i, entry in enumerate(entries):
+                if not (i in relevant_indeces):
+                    entries_kill_list.append(entry)
+
+            for entry in entries_kill_list:
+                x_legend.remove(entry)
+
+            for row in x_rows:
+                values_kill_list = []
+                values = row.findall("v")
+
+                for i, entry in enumerate(values):
+                    if not (i in relevant_indeces):
+                        values_kill_list.append(entry)
+                
+                for value in values_kill_list:
+                    row.remove(value)
+
+            x_metadata.find('columns').text = str(len(relevant_indeces))
+
+            xml_string = ET.tostring(xml_root, encoding='utf8', method='xml')
+            return Response(content=xml_string, media_type='application/xml')
 
         return dict(
             success=True,
